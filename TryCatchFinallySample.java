c2a067df52d9ffd5a56f801263bda7ffc64945d4
Grace <gracekanmei@gmail.com>
2023-07-12 16:16:17 +0900

    add

--- TryCatchFinallySample.java
+++ TryCatchFinallySample.java
@@ -1,13 +1,12 @@
 public class TryCatchFinallySample {
-
-	public static void main(String[] args) {
-		① {
-			System.out.println("①");
-			Integer.parseInt(args[0]);
-		} ② (Exception e) {
-			System.out.println("②");
-		} ③ {
-			System.out.println("③");
-		}
-	}
+    public static void main(String[] args) {
+        try {
+            System.out.println("Try");
+            Integer.parseInt(args[0]);
+        } catch (Exception e) {
+            System.out.println("Catch");
+        } finally {
+            System.out.println("Finally");
+        }
+    }
 }
--- TryCatchFinallySample2.java
+++ TryCatchFinallySample2.java
@@ -1,3 +1,18 @@
 public class TryCatchFinallySample2 {
+
+    private static Logger logger = Logger.getLogger(TryCatchFinallySample2.class.getName());
     
-}
+        public static void main(String[] args) {
+            logger.setLevel(Level.ALL);
+    
+            ① {
+                logger.②("①");
+                Integer.parseInt(args[0]);
+            } ③ (Exception e) {
+                logger.④("③");
+            } ⑤ {
+                logger.⑥("⑤");
+            }
+        }
+    }
+    
