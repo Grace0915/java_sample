0555bda8775ada1795010ca7cab3d5ad4ed52cab
Grace <gracekanmei@gmail.com>
2023-07-12 16:29:11 +0900

    add

--- TryCatchFinallySample2.java
+++ TryCatchFinallySample2.java
@@ -1,18 +1,19 @@
+import java.util.logging.*;
+
 public class TryCatchFinallySample2 {
 
-    private static Logger logger = Logger.getLogger(TryCatchFinallySample2.class.getName());
-    
-        public static void main(String[] args) {
-            logger.setLevel(Level.ALL);
-    
-            ① {
-                logger.②("①");
-                Integer.parseInt(args[0]);
-            } ③ (Exception e) {
-                logger.④("③");
-            } ⑤ {
-                logger.⑥("⑤");
-            }
-        }
-    }
-    
+private static Logger logger = Logger.getLogger(TryCatchFinallySample2.class.getName());
+
+	public static void main(String[] args) {
+		logger.setLevel(Level.ALL);
+
+		try {
+			logger.warning("try");
+			Integer.parseInt(args[0]);
+		} catch (Exception e) {
+			logger.severe("try");
+		} finally {
+			logger.info("finally");
+		}
+	}
+}
